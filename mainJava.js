7ba2e59b907d5f1f28c1e3473eab58d87e71a9c7
Tony <86742529+tony1610@users.noreply.github.com>
2022-12-04 07:55:08 -0500

    Update mainJava.js
    Nav menu adjustment

--- mainJava.js
+++ mainJava.js
@@ -7,11 +7,19 @@ const containerOverlay = document.querySelector(".container")
 navExpand.addEventListener("click", e =>{
     ulNav.classList.add("expand")
     containerOverlay.classList.add("overlay")
+    
+    navExpand.style.display = "none"
+    
     navClose.style.opacity = 1
+    navClose.style.zIndex = 88888
 })
 
 navClose.addEventListener("click", e =>{
     ulNav.classList.remove('expand')
     containerOverlay.classList.remove("overlay")
+    
+    navExpand.style.display = document.querySelector('hamburger-btn')
+    
     navClose.style.opacity = 0
+    navClose.style.zIndex = -1
 })
